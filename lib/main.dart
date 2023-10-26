61da70fe6fda0d7df5436f9cd645c89acc098be6
jecastanosm7 <jecastanosm@concredito.com.mx>
2023-10-26 16:16:57 -0700

    cambios en opacidad

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
 
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Mi boda',
       home: const MyHomePage(title: 'My Wedding'),
     );
   }
